0997f2d65ac495e7468d654ab4fb6d93fea14082
labba <labbala@gmail.com>
2019-11-27 00:15:11 +0200

    Add files via upload

--- demo.c
+++ demo.c
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
     printf("%d",argc);
     volatile int len = strlen(argv[1]);
 
-    if(argc + len < argc && len > 0)
+    if(argc + len < argc && len => 0)
         printf("smaller");
     else
         printf("bigger");
